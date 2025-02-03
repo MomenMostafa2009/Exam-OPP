59af29f60512e8f9baec8ce5b0716da8ca49fc6c
MomenMostafa2009 <momendarsh2009@gmail.com>
2025-02-03 18:41:17 +0200

    Add project files.

---
+++ Exam.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Exam.q;
+
+namespace Exam
+{
+    internal class Ex
+    {
+        public abstract class Exam
+        {
+            public DateTime TimeOfExam {get;set;}
+            public int NumberOfQuestions {get;set;}
+            public List<Question> Questions {get;set;} = new List<Question>();
+            public Exam(DateTime timeOfExam,int numberOfQuestions)
+            {
+                TimeOfExam = timeOfExam;
+                NumberOfQuestions = numberOfQuestions;
+            }
+
+            public abstract void ShowExam();
+        }
+        public class FinalExam:Exam
+        {
+            public FinalExam(DateTime timeOfExam, int numberOfQuestions): base(timeOfExam, numberOfQuestions)
+            {
+            }
+
+            public override void ShowExam()
+            { }
+        }
+        public class PracticalExam : Exam
+        {
+            public PracticalExam(DateTime timeOfExam, int numberOfQuestions) : base(timeOfExam, numberOfQuestions)
+            { }
+            public override void ShowExam()
+            { }
+        }
+        public class Subject
+        { public int SubjectId {get;set;}
+            public string SubjectName {get;set;}
+            public Exam SubjectExam {get;set;}
+
+            public Subject(int subjectId,string subjectName)
+            {
+                SubjectId = subjectId;
+                SubjectName = subjectName;
+            }
+
+            public void CreateExam(Exam exam)
+            {
+                SubjectExam = exam;
+            }
+        }
+
+
+    }
+}
---
+++ Question.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    internal class q
+    {
+        public abstract class Question
+        {
+            public string Header {get;set;}
+            public string Body {get;set;}
+            public int Mark {get;set;}
+            public Answer[] AnswerList {get;set;}
+            public Answer RightAnswer {get;set;}
+
+            public Question(string header,string body,int mark)
+            {
+                Header = header;
+                Body = body;
+                Mark = mark;
+            }
+            public abstract void ShowQuestion();
+        }
+        public class FinalQuestion:Question
+        { public enum QuestionType {TrueFalse,MCQ}
+            public QuestionType Type {get;set;}
+
+            public FinalQuestion(string header, string body, int mark, QuestionType type): base(header, body, mark)
+            {Type = type;}
+
+            public override void ShowQuestion()
+            {
+            }
+        }
+        public class PracticalQuestion:Question
+        {
+            public PracticalQuestion(string header, string body, int mark): base(header, body, mark)
+            {
+            }
+            public override void ShowQuestion()
+            {
+            }
+        }
+        public class Answer
+        {public int AnswerId { get; set; }
+         public string AnswerText { get; set; }
+
+            public Answer(int answerId,string answerText)
+            {
+                AnswerId = answerId;
+                AnswerText = answerText;
+            }
+        }
+
+
+    }
+}
